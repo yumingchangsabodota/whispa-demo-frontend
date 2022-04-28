933c6595a41bdf9896ebd2a915cb64f4f3f32c6c
Yu-Ming Chang <yu-ming.chang@odt.net>
2022-04-28 22:30:04 +0800

    checkpoint

--- src/lib/whispApp.js
+++ src/lib/whispApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ApiPromise } from '@polkadot/api'
 
 import Form from 'react-bootstrap/Form';
@@ -12,18 +12,30 @@ function WhispApp (){
     const whisp_cards = [];
 
     const node = NodeConnect("ws://127.0.0.1:9945");
-    
+
     const [whispTextEnable, setWhispTextEnable] = useState(true);
 
     const [walletSelected, setWalletSelected] = useState(undefined);
 
     const [whispValue, setwhispValue] = useState("");
 
-    const handleWhisp = (e) => {
+    const [queriedWhisp, setQueriedWhisp] = useState([]);
+
+    const [queriedWhispIds, setQueriedWhispIds] = useState([]);
+
+    const parseWhisp = ({gssip_id,whisper,timestamp,content}) => ({gssip_id:gssip_id.toJSON(),
+                                                                    whisper:whisper.toJSON(),
+                                                                    timestamp:timestamp.toJSON(),
+                                                                    content:content.toJSON()})
+
+    const handleWhisp = async (e) => {
         console.log(walletSelected);
         console.log(whispValue);
         clearWhisp(e);
-        getWalletBalance(walletSelected.address, node);
+        let whisp_ids = await getWalletBalance(walletSelected.address, node);
+        console.log(whisp_ids);
+        setQueriedWhispIds(whisp_ids);
+        //console.log(queriedWhispIds);
     }
 
     const clearWhisp = (e) => {
@@ -56,6 +68,19 @@ function WhispApp (){
         console.log(`${address} has a balance of ${previousFree}, nonce ${previousNonce}`);
         console.log(`You may leave this example running and start example 06 or transfer any value to ${address}`);
 
+        const [{ nonce: accountNonce }, now, entries] = await Promise.all([
+            api.query.system.account(address),
+            api.query.timestamp.now(),
+            await api.query.palletWhisper.whisps.entries()
+          ]);
+        
+        console.log(`accountNonce(${address}) ${accountNonce}`);
+        console.log(`last block timestamp ${now.toNumber()}`);
+        console.log(entries);
+        const ids = entries.map(entry => entry[1].unwrap().toJSON());
+        console.log(ids[0]);
+
+        return ids[0]
     }
 
     return(<>
@@ -71,6 +96,11 @@ function WhispApp (){
                     Whisp!
                 </Button>
             </Form>
+            <p>
+                {
+                    queriedWhispIds
+                }
+            </p>
         </div>
     </>)
 
